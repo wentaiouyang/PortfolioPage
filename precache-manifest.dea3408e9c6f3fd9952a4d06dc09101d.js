2f3c99f23cf9405183fa2ee118293b04e29af2dd
wentaiouyang <wentaiouyang@gmail.com>
2020-10-09 16:59:22 +1000

    Update

---
+++ precache-manifest.dea3408e9c6f3fd9952a4d06dc09101d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9af63208bc96187e28a5793b4061335",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27610c617aa164304300",
+    "url": "/static/css/main.d6f38b30.chunk.css"
+  },
+  {
+    "revision": "07e1f0eb79aee6d647fb",
+    "url": "/static/js/2.532c684e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.532c684e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27610c617aa164304300",
+    "url": "/static/js/main.b3878ca2.chunk.js"
+  },
+  {
+    "revision": "74ca039aabce4e4a9a3c",
+    "url": "/static/js/runtime-main.d7e80684.js"
+  }
+]);
